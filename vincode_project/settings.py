071077134f58543f75f76dc5aa42f8e17a932dc5
idris <idris@labrin.net>
2019-12-06 17:27:02 +0400

    change image sizes

--- vincode_project/settings.py
+++ vincode_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!2toz6pixcpot!h&tpp#qb_y&y_2pesmxz^pq1wotm)%5%n=zn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
